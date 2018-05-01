9ffa64ef259d5582a9969ac0f108f630a93d269c
Phill Conrad <pconrad@cs.ucsb.edu>
2018-05-01 16:43:45 -0700

    end of lecture

--- brokenFuncs.py
+++ brokenFuncs.py
@@ -16,21 +16,21 @@ def findMin(somelist):
     
 def brokeMax1(somelist):
     winner = somelist[0]
-    for i in range(1,len(someist)):
+    for i in range(1,len(somelist)):
         if somelist[i] > winner:
             winner = somelist[i]
         return winner
 
 def brokeMax2(somelist):
     winner = somelist[0]
-    for i in range(1,len(someist)):
+    for i in range(1,len(somelist)):
         if somelist[i] > winner:
             winner = somelist[i]
             return winner
 
 def brokeMax3(somelist):
     winner = somelist[0]
-    for i in range(1,len(someist)):
+    for i in range(1,len(somelist)):
         if somelist[i] > winner:
             winner = i
     return winner
